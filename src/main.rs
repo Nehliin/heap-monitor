2a59e100447962a0707e03fa8fc30b3a0a12a1ee
Nehliin <oskar.d.nehlin@gmail.com>
2022-04-03 14:56:55 +0200

    Calc top 10 largest outstanding allocations

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use futures::stream::StreamExt;
 use std::env;
 use std::process;
 use std::ptr;
-use std::time::Instant;
 use tokio::signal;
 use tokio::sync::mpsc::Sender;
 use tracing::{error, Level};
@@ -20,20 +19,19 @@ use probes::malloc::MallocEvent;
 
 use crate::syms::SymLoader;
 
-struct Allocation {
-    ptr: u64,
-    stack_id: i64,
-    size: u64,
-    frames: BpfStackFrames,
-    timestamp: Instant,
-}
-
 enum BpfEvent {
-    Allocation(Allocation),
-    Free(u64),
+    Allocation {
+        ptr: u64,
+        stack_id: i64,
+        size: u64,
+        frames: Box<BpfStackFrames>,
+    },
+    Free {
+        ptr: u64,
+    },
 }
 
-async fn start_perf_event_handler(mut loaded: Loaded, sender: Sender<BpfEvent>) {
+async fn event_handler(mut loaded: Loaded, sender: Sender<BpfEvent>) {
     while let Some((name, events)) = loaded.events.next().await {
         match name.as_str() {
             "malloc_event" => {
@@ -42,13 +40,12 @@ async fn start_perf_event_handler(mut loaded: Loaded, sender: Sender<BpfEvent>) 
                     let mut stack_trace = StackTrace::new(loaded.map("stack_trace").unwrap());
                     if let Some(frames) = stack_trace.get(m_event.stackid) {
                         sender
-                            .send(BpfEvent::Allocation(Allocation {
+                            .send(BpfEvent::Allocation {
                                 ptr: m_event.ptr,
                                 stack_id: m_event.stackid,
                                 size: m_event.size,
-                                timestamp: Instant::now(),
-                                frames,
-                            }))
+                                frames: Box::new(frames),
+                            })
                             .await
                             .map_err(|_err| "Failed to send allocation event")
                             .unwrap();
@@ -60,9 +57,8 @@ async fn start_perf_event_handler(mut loaded: Loaded, sender: Sender<BpfEvent>) 
             "free_event" => {
                 for event in events {
                     let f_event = unsafe { ptr::read(event.as_ptr() as *const MallocEvent) };
-                    println!("Freed! {}", f_event.ptr);
                     sender
-                        .send(BpfEvent::Free(f_event.ptr))
+                        .send(BpfEvent::Free { ptr: f_event.ptr })
                         .await
                         .map_err(|_err| "Failed to send deallocation event")
                         .unwrap();
@@ -102,7 +98,7 @@ async fn main() {
 
     let (tx, mut rc) = tokio::sync::mpsc::channel(512);
 
-    let event_handler = tokio::spawn(async move {
+    let event_handler_task = tokio::spawn(async move {
         let mut loaded = Loader::load(probe_code()).expect("error loading BPF program");
 
         for prb in loaded.uprobes_mut() {
@@ -110,45 +106,92 @@ async fn main() {
                 .unwrap_or_else(|_| panic!("error attaching uprobe program {}", prb.name()));
         }
 
-        println!(
-            "Monitoring heap in PID {}, Hit Ctrl-C to quit!",
-            args.pid
-        );
+        println!("Monitoring heap in PID {}, Hit Ctrl-C to quit!", args.pid);
         tokio::select! {
             _ = signal::ctrl_c() => {},
-            _ = start_perf_event_handler(loaded, tx) => {}
+            _ = event_handler(loaded, tx) => {}
         }
     });
 
-    let processor = tokio::spawn(async move {
+    let event_proccesing_task = tokio::spawn(async move {
+        let mut allocations = AHashMap::new();
+        let mut stack_traces = AHashMap::new();
+        let mut allocation_stats: AHashMap<i64, AllocationStat> = AHashMap::new();
+
+        struct AllocationStat {
+            alloc_count: u64,
+            total_size: u64,
+        }
+
+        struct Allocation {
+            size: u64,
+            stack_id: i64,
+        }
+
+        while let Some(event) = rc.recv().await {
+            match event {
+                BpfEvent::Allocation {
+                    ptr,
+                    stack_id,
+                    size,
+                    frames,
+                } => {
+                    stack_traces.insert(stack_id, frames);
+                    allocations.insert(ptr, Allocation { size, stack_id });
+                    if let Some(stats) = allocation_stats.get_mut(&stack_id) {
+                        stats.alloc_count += 1;
+                        stats.total_size += size;
+                    } else {
+                        allocation_stats.insert(
+                            stack_id,
+                            AllocationStat {
+                                alloc_count: 0,
+                                total_size: size,
+                            },
+                        );
+                    }
+                }
+                BpfEvent::Free { ptr } => {
+                    if let Some(Allocation { size, stack_id }) = allocations.remove(&ptr) {
+                        if let Some(stats) = allocation_stats.get_mut(&stack_id) {
+                            stats.alloc_count -= 1;
+                            stats.total_size += size;
+                        }
+                    }
+                }
+            }
+        }
+
         let mut symloader = SymLoader::new(pid);
         let mut modules = symloader.load_modules();
 
         let mut cache = AHashMap::new();
         let stdout = std::io::stdout();
 
-        let mut allocations = AHashMap::new();
-
-        while let Some(event) = rc.recv().await {
-            match event {
-                BpfEvent::Allocation(allocation) => allocations.insert(allocation.ptr, allocation),
-                BpfEvent::Free(ptr) => allocations.remove(&ptr),
-            };
-        }
         println!("Proccessing..");
         let start = std::time::Instant::now();
-        let mut allocations: Vec<Allocation> = allocations.into_iter().map(|(_, v)| v).collect();
-        allocations.sort_unstable_by(|a, b| a.timestamp.cmp(&b.timestamp));
 
-        for allocation in allocations.iter() {
-            modules.addr_to_line(&allocation.frames.ip, &mut cache, &stdout);
+        let mut largest_outstanding: Vec<(i64, AllocationStat)> =
+            allocation_stats.into_iter().collect();
+        largest_outstanding.sort_unstable_by(|(_, a), (_, b)| a.total_size.cmp(&b.total_size));
+
+        for (stack_id, alloc_stat) in largest_outstanding.iter().take(10) {
+            if let Some(frames) = stack_traces.get(stack_id) {
+                println!(
+                    "Allocated a total of {} bytes from {} separate allocation",
+                    alloc_stat.total_size, alloc_stat.alloc_count
+                );
+                modules.addr_to_line(&frames.ip, &mut cache, &stdout);
+            } else {
+                tracing::error!("No stack trace found for {stack_id}");
+            }
         }
         let duration = start.elapsed().as_millis();
         println!("Time taken {duration}");
     });
 
-    let _ = event_handler.await.unwrap();
-    let _ = processor.await.unwrap();
+    let _ = event_handler_task.await.unwrap();
+    let _ = event_proccesing_task.await.unwrap();
 }
 
 fn probe_code() -> &'static [u8] {
